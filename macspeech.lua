a864c880321449da9510e85035dae8a50bbb74ee
Quin <quin.marilyn05@gmail.com>
2022-11-09 06:28:36 -0700

    Do some really ugly hackery to get and use the system voice by default. This is so ugly and i hate it.

--- macspeech.lua
+++ macspeech.lua
@@ -3,6 +3,8 @@ local objc = require("objc.objc")
 objc.load("AVFoundation")
 local backend = {}
 backend.synth = objc.AVSpeechSynthesizer:alloc():init()
+-- Because AVSpeechSynthesizer doesn't give us the proper default voice (it returned ALbert on my system for some reason), we have to do this bit of uglyness with NSSpeechSynthesizer.
+backend.voice = objc.NSSpeechSynthesizer:alloc():init():defaultVoice()
 
 function backend.output(text, interrupt)
 	text = tostring(text)
@@ -11,6 +13,7 @@ function backend.output(text, interrupt)
 		backend.synth:stopSpeakingAtBoundary(0)
 	end
 	local utterance = objc.AVSpeechUtterance:alloc():initWithString(text)
+	utterance.voice = objc.AVSpeechSynthesisVoice:voiceWithIdentifier(backend.voice)
 	backend.synth:speakUtterance(utterance)
 end
 
